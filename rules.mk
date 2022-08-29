ed48825147883d1a381eecf4c8428e08755123f4
Manfred Touron <94029+moul@users.noreply.github.com>
2022-08-29 22:34:24 +0000

    feat: add faucet stats
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- rules.mk
+++ rules.mk
@@ -27,6 +27,12 @@ stats:
 	echo '```' >> README.md
 	echo >> README.md
 
+	echo "## top faucet requesters" >> README.md
+	echo '```' >> README.md
+	cat txexport*.log | jq -r '.msg[] | select(.["@type"]=="/bank.MsgSend") | select(.["from_address"]=="g127jydsh6cms3lrtdenydxsckh23a8d6emqcvfa") | .to_address + " " + .amount' | sed 's/ugnot$$//' | awk 'NR == 1 {next} {a[$$1] += $$2} {b[$$1] += 1} END {for (i in a) {if (a[i] >= 500000000){printf "%-15s\t%s\t%s\n", i, b[i], a[i]}}}' | sort -rnk2 >> README.md
+	echo '```' >> README.md
+	echo >> README.md
+
 loop:
 	while true; do \
 		( \
