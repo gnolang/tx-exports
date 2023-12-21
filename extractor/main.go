b56850ae8d3135a84fbc3833818c3993a99f9250
Leon Hudak <33522493+leohhhn@users.noreply.github.com>
2023-12-21 08:51:42 +0100

    remove unused anon vars
    Co-authored-by: Antonio Navarro Perez <antnavper@gmail.com>

--- extractor/main.go
+++ extractor/main.go
@@ -51,7 +51,7 @@ func main() {
 
 	// Run the command
 	if err := cmd.ParseAndRun(context.Background(), os.Args[1:]); err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "%+v", err)
+		fmt.Fprintf(os.Stderr, "%+v", err)
 
 		os.Exit(1)
 	}
