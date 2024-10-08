3971481df66de356fceb85015409c8362348693a
Sergio Maria Matone <sw360cab@gmail.com>
2024-10-08 10:55:34 +0200

    rehauling backup workflow

--- portal-loop/export.sh
+++ portal-loop/export.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+set -xe
+
 # Define the constants
 TMP_DIR=temp-gno # Temporary directory for the working data
-BACKUP_NAME=backup_portal.loop.$(date +%s).jsonl
 GENESIS=genesis.json
 WGET_OUTPUT=wget-genesis.json
 BACKUP_PREFIX=backup_
+BACKUP_NAME=backup_portal_loop.jsonl
+# Latest backup is commited in the repo
+LATEST_BACKUP_FILE=../"$BACKUP_NAME"
 
 # Create the temporary working dir
 mkdir $TMP_DIR
@@ -29,50 +33,38 @@ cd ../.. # move back to the portal-loop directory
 # Clean up the downloaded genesis.json and the wget response
 rm $GENESIS $WGET_OUTPUT
 
-# Find the latest backup file based on the Unix timestamp in the filename
-LATEST_BACKUP_FILE=$(ls ../"$BACKUP_PREFIX"*.jsonl 2>/dev/null | sort -t'-' -k2,2n | tail -n 1)
-
 # Check if there is an existing backup
-if [[ -z "$LATEST_BACKUP_FILE" ]]; then
+if [[ ! -f "$LATEST_BACKUP_FILE" ]]; then
   # Save the initial backup
   echo "Saving initial backup to $BACKUP_NAME"
 
   # Make the backup file official
-  cp "$BACKUP_NAME" ../"$BACKUP_NAME"
+  cp "$BACKUP_NAME" "$LATEST_BACKUP_FILE"
+else # Backup file exists!
+  LATEST_BACKUP_SORTED=latest_backup_sort.jsonl
+  DIFF_TXS=diff.jsonl
 
-  # Remove the temporary working directory
-  cd .. || exit 1
-  rm -rf $TMP_DIR
+  # There is an existing backup already, check it
+  echo "Backup file already present in the repository"
+
+  # Sort the latest backup file
+  sort "$LATEST_BACKUP_FILE" > "$LATEST_BACKUP_SORTED"
   
-  exit 0
-fi
+  # Sort the latest genesis tx sheet
+  sort "$BACKUP_NAME" > temp_"$BACKUP_NAME"
 
-LATEST_BACKUP_SORTED=latest_backup_sort.jsonl
-DIFF_TXS=diff.jsonl
+  # Use comm to find lines only in file2 (additions) and write to output file
+  comm -13 ./"$LATEST_BACKUP_SORTED" temp_"$BACKUP_NAME" > "$DIFF_TXS"
 
-# There is an existing backup already, check it
-echo "Latest backup file: $LATEST_BACKUP_FILE"
-
-# Sort the latest backup file
-sort "$LATEST_BACKUP_FILE" > "$LATEST_BACKUP_SORTED"
-
-# Sort the latest genesis tx sheet
-sort "$BACKUP_NAME" > temp_"$BACKUP_NAME"
-
-# Clean up the temporary backup file
-rm "$BACKUP_NAME"
-
-
-# Use comm to find lines only in file2 (additions) and write to output file
-comm -13 ./"$LATEST_BACKUP_SORTED" temp_"$BACKUP_NAME" > "$DIFF_TXS"
-
-# Notify if differences were found
-if [[ -z $(grep '[^[:space:]]' "$DIFF_TXS") ]]; then
-  echo "No differences found."
-else
-  echo "Differences found."
-  cp "$DIFF_TXS" ../"$BACKUP_NAME"
-  echo "Differences saved to $BACKUP_NAME"
+  # Notify if differences were found
+  if [[ -z $(grep '[^[:space:]]' "$DIFF_TXS") ]]; then
+    echo "No differences found. Exiting with no further activities."
+  else
+    echo "Differences found. Replacing backup file"
+    # Make the backup file official
+    cp "$BACKUP_NAME" "$LATEST_BACKUP_FILE"
+    echo "Stored a new backup file"
+  fi
 fi
 
 cd .. || exit 1
--- portal-loop/extracted/r/abach/raffle/gno.mod
+++
@@ -1,5 +0,0 @@
-module gno.land/r/gc24/alexander-bachmann/raffle
-
-require (
-    gno.land/r/gc24/raffle v0.0.0-latest
-)
